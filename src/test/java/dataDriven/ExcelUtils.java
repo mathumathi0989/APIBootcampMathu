8a95950b976c9adcbda78a71911ac7834e884a9b
Mathumathi Balakrishnan <mathumathi.b@gmail.com>
2024-06-24 16:15:43 -0400

    completed crud using testNG

--- src/test/java/dataDriven/ExcelUtils.java
+++ src/test/java/dataDriven/ExcelUtils.java
@@ -16,73 +16,72 @@ import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 
 public class ExcelUtils {
 
-	  private String filePath;
+	private String filePath;
 
-	    public ExcelUtils(String filePath) {
-	        this.filePath = filePath;
-	    }
+    public ExcelUtils(String filePath) {
+        this.filePath = filePath;
+    }
 
-	    public List<Map<String, String>> getUserData(String sheetName) {
-	        List<Map<String, String>> userDataList = new ArrayList<>();
-	        try (FileInputStream fis = new FileInputStream(filePath);
-	             Workbook workbook = new XSSFWorkbook(fis)) {
+    public List<Map<String, Object>> getUserData(String sheetName) {
+        List<Map<String, Object>> userDataList = new ArrayList<>();
+        try (FileInputStream fis = new FileInputStream(filePath);
+             Workbook workbook = new XSSFWorkbook(fis)) {
 
-	            Sheet sheet = workbook.getSheet(sheetName);
-	            int rowCount = sheet.getPhysicalNumberOfRows();
+            Sheet sheet = workbook.getSheet(sheetName);
+            int rowCount = sheet.getPhysicalNumberOfRows();
 
-	            for (int i = 1; i < rowCount; i++) { // Assuming first row is header
-	                Row row = sheet.getRow(i);
-	                Map<String, String> userData = new HashMap<>();
+            for (int i = 1; i < rowCount; i++) { // Assuming first row is header
+                Row row = sheet.getRow(i);
+                Map<String, Object> userData = new HashMap<>();
 
-	                userData.put("user_first_name", getCellValue(row.getCell(0)));
-	                userData.put("user_last_name", getCellValue(row.getCell(1)));
-	                userData.put("user_contact_number", getCellValue(row.getCell(2)));
-	                userData.put("user_email_id", getCellValue(row.getCell(3)));
+                userData.put("user_first_name", getCellValue(row.getCell(0)));
+                userData.put("user_last_name", getCellValue(row.getCell(1)));
+                userData.put("user_contact_number", getCellValue(row.getCell(2)));
+                userData.put("user_email_id", getCellValue(row.getCell(3)));
 
-	                Map<String, String> userAddress = new HashMap<>();
-	                userAddress.put("plotNumber", getCellValue(row.getCell(4)));
-	                userAddress.put("street", getCellValue(row.getCell(5)));
-	                userAddress.put("state", getCellValue(row.getCell(6)));
-	                userAddress.put("country", getCellValue(row.getCell(7)));
-	                userAddress.put("zipCode", getCellValue(row.getCell(8)));
+                Map<String, String> userAddress = new HashMap<>();
+                userAddress.put("plotNumber", getCellValue(row.getCell(4)));
+                userAddress.put("street", getCellValue(row.getCell(5)));
+                userAddress.put("state", getCellValue(row.getCell(6)));
+                userAddress.put("country", getCellValue(row.getCell(7)));
+                userAddress.put("zipCode", getCellValue(row.getCell(8)));
 
-	                userData.put("userAddress", userAddress.toString());
-	                userDataList.add(userData);
-	            }
-	        } catch (IOException e) {
-	            e.printStackTrace();
-	        }
-	        return userDataList;
-	    }
+                userData.put("userAddress", userAddress);
+                userDataList.add(userData);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return userDataList;
+    }
 
-	    private String getCellValue(Cell cell) {
-	        if (cell == null) {
-	            return "";
-	        }
+    private String getCellValue(Cell cell) {
+        if (cell == null) {
+            return "";
+        }
 
-	        switch (cell.getCellType()) {
-	            case STRING:
-	                return cell.getStringCellValue();
-	            case NUMERIC:
-	                if (DateUtil.isCellDateFormatted(cell)) {
-	                    return cell.getDateCellValue().toString();
-	                } else {
-	                    double numericValue = cell.getNumericCellValue();
-	                    if (numericValue == (int) numericValue) {
-	                        return String.valueOf((int) numericValue); // Convert to integer if it is a whole number
-	                    } else {
-	                        return String.valueOf(numericValue);
-	                    }
-	                }
-	            case BOOLEAN:
-	                return String.valueOf(cell.getBooleanCellValue());
-	            case FORMULA:
-	                return cell.getCellFormula();
-	            case BLANK:
-	                return "";
-	            default:
-	                return "";
-	        }
-	    }
-	    
+        switch (cell.getCellType()) {
+            case STRING:
+                return cell.getStringCellValue();
+            case NUMERIC:
+                if (DateUtil.isCellDateFormatted(cell)) {
+                    return cell.getDateCellValue().toString();
+                } else {
+                    double numericValue = cell.getNumericCellValue();
+                    if (numericValue == (int) numericValue) {
+                        return String.valueOf((int) numericValue); // Convert to integer if it is a whole number
+                    } else {
+                        return String.valueOf(numericValue);
+                    }
+                }
+            case BOOLEAN:
+                return String.valueOf(cell.getBooleanCellValue());
+            case FORMULA:
+                return cell.getCellFormula();
+            case BLANK:
+                return "";
+            default:
+                return "";
+        }
+    }
 }
---
+++ src/test/java/dataDriven/RequestUsingExcel.java
@@ -0,0 +1,119 @@
+package dataDriven;
+
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.containsString;
+import static org.hamcrest.Matchers.equalTo;
+
+import java.util.List;
+import java.util.Map;
+
+import org.testng.Assert;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import io.restassured.RestAssured;
+import io.restassured.http.ContentType;
+import io.restassured.response.Response;
+
+public class RequestUsingExcel {
+
+	 protected String baseURI;
+     protected String username;
+     protected String password;
+     public int userId;
+     public int userCount;
+     public int AfterPostuserCount;
+
+     private ExcelUtils excelUtils;
+     private List<Map<String, Object>> usersData;
+
+     @BeforeTest
+     public void setup() {
+         baseURI = "https://userserviceapi-a54ceee3346a.herokuapp.com/uap/";
+         RestAssured.baseURI = baseURI;
+         username = "Numpy@gmail.com";
+         password = "userapi123";
+         excelUtils = new ExcelUtils("C:/Users/mathu/Downloads/APIDummyData.xlsx"); // Path to your Excel file
+         usersData = excelUtils.getUserData("Sheet1"); // Read all user data from the sheet
+     }
+
+@Test
+     public void testCRUDUsers() {
+    	 ObjectMapper objectMapper = new ObjectMapper(); // Create ObjectMapper instance
+    	 for (Map<String, Object> userData : usersData) {
+    		 //Get all users
+    		 Response response = given().auth().basic(username, password).when().get("users").then().statusCode(200).extract().response();
+             userCount = response.jsonPath().getList("users").size();
+             System.out.println("Total Number of users: " + userCount);
+             System.out.println("-----------------------------------------------------------------------------------------------------------");
+       
+             // Create user
+             
+             try {
+            
+                 String jsonBody = objectMapper.writeValueAsString(userData);
+
+                 System.out.println("++++++++++++++++++++++++++++++++++++++++++++");
+                 System.out.println(jsonBody);
+                 System.out.println("++++++++++++++++++++++++++++++++++++++++++++");
+
+                 Response createresponse = given()
+                         .auth().basic(username, password)
+                         .contentType(ContentType.JSON)
+                         .body(jsonBody)
+                         .when().post("createusers")
+                         .then().log().all().statusCode(201).extract().response();
+
+                 userId = createresponse.jsonPath().getInt("user_id");
+                 System.out.println("Created user ID: " + userId);
+                 System.out.println("-----------------------------------------------------------------------------------------------------------");
+             } catch (Exception e) {
+                 e.printStackTrace();
+             }
+       
+    //Get by id
+        Response getresponse = given().auth().basic(username, password).when().get("users").then().statusCode(200).extract().response();
+         AfterPostuserCount = getresponse.jsonPath().getList("users").size();
+         System.out.println("Total Number of users: " + AfterPostuserCount);
+         int count = AfterPostuserCount - userCount;
+         Assert.assertEquals(count, 1); 
+         System.out.println("-----------------------------------------------------------------------------------------------------------");
+    
+//update user
+
+
+             try {
+                 // Convert Map to JSON string
+                 String jsonBody = objectMapper.writeValueAsString(userData);
+
+                 Response updateresponse = given()
+                         .auth().basic(username, password)
+                         .contentType(ContentType.JSON)
+                         .body(jsonBody)
+                         .when().put("updateuser/" + userId)
+                         .then().log().all()                     
+                         .statusCode(200).extract().response();
+
+                 String body = updateresponse.getBody().asString();
+                 System.out.println("Response Body is " + body);
+                 
+                 System.out.println("-----------------------------------------------------------------------------------------------------------");
+             } catch (Exception e) {
+                 e.printStackTrace();
+             }
+         
+     
+
+  //delete user
+     
+             Response deleteresponse = given().auth().basic(username, password).when().delete("deleteuser/" + userId).then().log().all().statusCode(200).extract().response();
+             System.out.println("delete response "+deleteresponse);
+             System.out.println("-----------------------------------------------------------------------------------------------------------");
+         }
+     
+     }
+}
